eff881423b66cb1c03927eae871baa4a63ca9e51
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-12 00:37:01 +0900

    ブログ記事投稿ページを作成しよう

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./CreatePost.module.css";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <h1>記事を投稿する</h1>
+        <div className={styles.inputPost}>
+          <h2>タイトル</h2>
+          <input type="text" placeholder="タイトルを記入" />
+        </div>
+        <div className={styles.inputPost}>
+          <h2>投稿</h2>
+          <textarea placeholder="投稿内容を記入"></textarea>
+        </div>
+        <button className={styles.postBtn}></button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
