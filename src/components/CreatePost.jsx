19834d10e67f8f2bdb0f0e197bc74fc6d4765145
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-13 00:14:17 +0900

    タイトルと記事内容の文字列を取得してみよう

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CreatePost.module.css";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
   return (
     <div className={styles.page}>
       <div className={styles.container}>
         <h1 className={styles.headtitle}>記事を投稿する</h1>
         <div className={styles.inputPost}>
           <h2>タイトル</h2>
-          <input type="text" placeholder="タイトルを記入" />
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className={styles.inputPost}>
           <h2>投稿</h2>
-          <textarea placeholder="投稿内容を記入"></textarea>
+          <textarea
+            placeholder="投稿内容を記入"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
         <button className={styles.postBtn}>投稿する</button>
       </div>
