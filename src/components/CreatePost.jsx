bca20f8578bce3fd5bb512766615d915ef165127
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-12 23:55:24 +0900

    タイトルと記事投稿にCSSを適用させよう

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -5,7 +5,7 @@ const CreatePost = () => {
   return (
     <div className={styles.page}>
       <div className={styles.container}>
-        <h1>記事を投稿する</h1>
+        <h1 className={styles.headtitle}>記事を投稿する</h1>
         <div className={styles.inputPost}>
           <h2>タイトル</h2>
           <input type="text" placeholder="タイトルを記入" />
@@ -14,7 +14,7 @@ const CreatePost = () => {
           <h2>投稿</h2>
           <textarea placeholder="投稿内容を記入"></textarea>
         </div>
-        <button className={styles.postBtn}></button>
+        <button className={styles.postBtn}>投稿する</button>
       </div>
     </div>
   );
