5625de8d539991d3101ce1376c7591c575e323af
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-14 00:02:46 +0900

    Homeコンポーネントを作成しよう

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="page">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </div>
+        <div className="postName">
+          <h3>@sasasa</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
