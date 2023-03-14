f3b8013f754b3a39b4c71ff2dc822dbb681ca21c
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-15 00:44:20 +0900

    HomeコンポーネントにCSSを適用してみよう

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className="page">
-      <div className="postContents">
-        <div className="postHeader">
+    <div className={styles.page}>
+      <div className={styles.postContents}>
+        <div className={styles.postHeader}>
           <h1>タイトル</h1>
         </div>
-        <div className="postTextContainer">
+        <div className={styles.postTextContainer}>
           テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
         </div>
-        <div className="postName">
+        <div className={styles.postName}>
           <h3>@sasasa</h3>
           <button>削除</button>
         </div>
