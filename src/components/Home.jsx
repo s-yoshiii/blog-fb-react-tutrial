472f3202dc44a8dc2cb36465c8bb6fb952adce9e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-17 00:49:54 +0900

    map関数でブログデータを1つずつ取り出して表示しよう

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 const Home = () => {
+  const [postList, setPostList] = useState([]);
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
       console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   }, []);
   return (
     <div className={styles.page}>
-      <div className={styles.postContents}>
-        <div className={styles.postHeader}>
-          <h1>タイトル</h1>
-        </div>
-        <div className={styles.postTextContainer}>
-          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-        </div>
-        <div className={styles.postName}>
-          <h3>@sasasa</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className={styles.postContents}>
+            <div className={styles.postHeader}>
+              <h1>{post.title}</h1>
+            </div>
+            <div className={styles.postTextContainer}>{post.postText}</div>
+            <div className={styles.postName}>
+              <h3>@ {post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
