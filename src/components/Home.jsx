c7bcb6962c96323662083f12669e72ef394d6489
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-19 01:16:37 +0900

    ブログ記事を削除する関数を作成しよう

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Home.module.css";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, getDocs, doc } from "firebase/firestore";
 import { db } from "../firebase";
 const Home = () => {
   const [postList, setPostList] = useState([]);
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   }, []);
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = "/";
+  };
   return (
     <div className={styles.page}>
       {postList.map((post) => {
         return (
-          <div className={styles.postContents}>
+          <div className={styles.postContents} key={post.id}>
             <div className={styles.postHeader}>
               <h1>{post.title}</h1>
             </div>
             <div className={styles.postTextContainer}>{post.postText}</div>
             <div className={styles.postName}>
               <h3>@ {post.author.username}</h3>
-              <button>削除</button>
+              <button onClick={() => handleDelete(post.id)}>削除</button>
             </div>
           </div>
         );
